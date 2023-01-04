119d7f357651b6c18018b99decbced39ff79a1d3
Emerson <emerson.day@gmail.com>
2023-01-03 17:34:35 -0800

    created ui layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 
 import Login from './components/login/Login'
-import logo from './logo.svg'
 import './App.css'
 import { AuthProvider } from './components/login/Auth'
 import Navbar from './components/login/Navbar'
 import Footer from './components/login/Footer'
+import CreatePlaylist from './components/login/CreatePlaylist'
 
 function App() {
   return (
     <AuthProvider>
     <div className='App'>
     <Navbar/>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <main className='App-main'>
+        <CreatePlaylist/>
         <Login/>
-      </header>
+      </main>
       <Footer/>
     </div>
     </AuthProvider>
