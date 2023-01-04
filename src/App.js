afd7099c4499a55b6daae14b3f9da7f105440be5
Emerson <emerson.day@gmail.com>
2023-01-03 16:22:53 -0800

    added nav

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Login from './components/login/Login'
 import logo from './logo.svg'
 import './App.css'
 import { AuthProvider } from './components/login/Auth'
+import Navbar from './components/login/Navbar'
 
 function App() {
   return (
     <AuthProvider>
     <div className='App'>
+    <Navbar/>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
