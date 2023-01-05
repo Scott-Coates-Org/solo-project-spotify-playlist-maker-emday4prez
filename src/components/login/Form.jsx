c87824e3858771de6bfb0fed83397dc1a4ab4a50
Emerson <emerson.day@gmail.com>
2023-01-04 16:32:57 -0800

    added select for year

--- src/components/login/Form.jsx
+++ src/components/login/Form.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import Select from 'react-select'
 import styles from './form.module.css'
-const options = [
-  { value: 'techno', label: 'Techno' },
-  { value: 'house', label: 'House' },
-  { value: 'disco', label: 'Disco' },
-  { value: 'ambient', label: 'Ambient' },
-  { value: 'tech house', label: 'Tech House' }
+
+const years = [];
+const currentYear = new Date().getFullYear();
+const genres = [
+  { value: 'jazz', label: 'Jazz' },
+  { value: 'blues', label: 'Blues' },
+  { value: 'rock', label: 'Rock-n-Roll' },
+  { value: 'folk', label: 'Folk' },
+  { value: 'country', label: 'Country' }
 ]
+
+for (let i = 0; i < 101; i++) {
+  years.push({ value: currentYear - i, label: currentYear - i });
+}
+
 
 
 const Form = () => (
-  <Select options={options} className={styles.select} />
+ <form>
+  <Select options={genres} placeholder="Select Genre..."   className={styles.select} />
+  <Select options={years} placeholder="Select Year..."   className={styles.select} />
+ </form>
+  
+ 
+ 
 )
 
 export default Form
