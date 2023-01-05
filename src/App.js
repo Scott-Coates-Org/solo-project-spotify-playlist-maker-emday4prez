d08d06b21f1f952d8da023323a6a21e1f79edf45
Emerson <emerson.day@gmail.com>
2023-01-04 22:44:59 -0800

    moved auth back to app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-
+import React from 'react'
 import Login from './components/login/Login'
 import './App.css'
 import Navbar from './components/login/Navbar'
 import Footer from './components/login/Footer'
 import CreatePlaylist from './components/login/CreatePlaylist'
+import { AuthProvider } from './components/login/Auth'
 
 function App() {
+
   return (
-   
-    <div className='App'>
-    <Navbar/>
-      <main className='App-main'>
-        <CreatePlaylist/>
-        <Login/>
-      </main>
-      <Footer/>
+   <AuthProvider>
+      <div className='App'>
+        <Navbar/>
+        <main className='App-main'>
+          <CreatePlaylist />
+          <Login/>
+        </main>
+        <Footer/>
     </div>
+   </AuthProvider>
+
     
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { AuthProvider } from './components/login/Auth'
+
 import './index.css'
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
-	<AuthProvider>
+	
 			<App />
-	</AuthProvider>
+
 	</React.StrictMode>,
 )
