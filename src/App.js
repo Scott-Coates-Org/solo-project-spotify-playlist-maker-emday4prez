7ea5871c241415e45dd4c6814a83f8cdb89699a6
Emerson <emerson.day@gmail.com>
2023-01-04 21:21:59 -0800

    moved auth context to index.js and created getUserData function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 
 import Login from './components/login/Login'
 import './App.css'
-import { AuthProvider } from './components/login/Auth'
 import Navbar from './components/login/Navbar'
 import Footer from './components/login/Footer'
 import CreatePlaylist from './components/login/CreatePlaylist'
 
 function App() {
   return (
-    <AuthProvider>
+   
     <div className='App'>
     <Navbar/>
       <main className='App-main'>
@@ -17,7 +16,7 @@ function App() {
       </main>
       <Footer/>
     </div>
-    </AuthProvider>
+    
   )
 }
 
